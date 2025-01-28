d18d03961141f342776cf6a9e427b6c0ba3fc438
Azamat <urazimbetovazamat6@gmail.com>
2025-01-29 03:32:33 +0500

    Initial commit

---
+++ app/src/main/java/com/example/simplecounterapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.simplecounterapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.simplecounterapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var binding : ActivityMainBinding
+    private var counter = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.button.setOnClickListener(){
+             counter++
+            binding.textView.text = counter.toString()
+        }
+    }
+}
